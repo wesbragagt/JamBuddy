f1de3708c121b32380c64767517fc3861c548d31
Weslley Braga <wesbragagt@gmail.com>
2019-04-20 21:37:12 -0500

    creates api and post to it

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080;
-var path = require("path");
+const express = require("express");
+const path = require("path");
+// express app
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
+// data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// routes
-app.get("/", function(req, res) {
+app.listen(PORT, function() {
+    console.log(`App is listening on port ${PORT}`);
+});
+
+// Routes
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "home.html"));
 });
-app.get("/survey", function(req, res) {
+app.get("/survey", (req, res) => {
     res.sendFile(path.join(__dirname, "survey.html"));
 });
 
-app.listen(PORT, function() {
-    console.log("APP is listening on port" + PORT);
+// sends everyone inside the api
+app.get("/api/buddy", (req, res) => {
+    res.json(buddy);
 });
+
+// post to the api buddy
+app.post("/api/buddy", (req, res) => {
+    const newBuddy = req.body;
+
+    newBuddy.routeName = newBuddy.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newBuddy);
+
+    buddy.push(newBuddy);
+});
+
+// api data test object
+
+const buddy = [
+    {
+        routeName: "ahmed",
+        name: "Ahmed",
+        photo:
+            "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    }
+];
